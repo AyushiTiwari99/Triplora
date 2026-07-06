5a59d2c994ad1a958f3276f48b9a75bf004edc90
AYUSHI TIWARI <tiwariayushi469@gmail.com>
2026-07-06 17:37:17 +0530

    Add category field to listings and seed database with default listings

--- init/index.js
+++ init/index.js
@@ -1,26 +1,39 @@
+if (process.env.NODE_ENV != "production") {
+  require('dotenv').config();
+}
+
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
+const User = require("../models/user.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/Triplora";
+const dbUrl = process.env.ATLASDB_URL;
 
 main()
-  .then(() => {
+  .then(async () => {
     console.log("connected to DB");
+    await initDB();
+    mongoose.connection.close();
   })
   .catch((err) => {
     console.log(err);
   });
 
 async function main() {
-  await mongoose.connect(MONGO_URL);
+  await mongoose.connect(dbUrl);
 }
 
 const initDB = async () => {
   await Listing.deleteMany({});
-  initData.data = initData.data.map((obj) => ({ ...obj, owner: "68f33a16200eab7336f30c90"}));
+  
+  let adminUser = await User.findOne({ username: "triplora_admin" });
+  if (!adminUser) {
+    const newUser = new User({ email: "admin@triplora.dev", username: "triplora_admin" });
+    const randomPassword = Math.random().toString(36).slice(-8) + Math.random().toString(36).toUpperCase().slice(-8);
+    adminUser = await User.register(newUser, randomPassword);
+  }
+
+  initData.data = initData.data.map((obj) => ({ ...obj, owner: adminUser._id }));
   await Listing.insertMany(initData.data);
   console.log("data was initialized");
 };
-
-initDB();
